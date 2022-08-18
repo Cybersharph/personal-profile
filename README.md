fff76c4b15e66f813b29a30b08a4b42c194050e6
Muyivu Shafiq <cybersharph@gmail.com>
2022-08-18 23:35:02 +0300

    Initial version

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is my practice react reposity
+
+## - Muyivu Shafiq
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+
+
+function Footer() {
+    return (
+        <footer className="bg-dark text-muted">
+            <small>
+                &copy; 2022. All rights reserved.
+            </small>
+            <hr />
+        </footer>
+    );
+}
+
+export default Footer;
